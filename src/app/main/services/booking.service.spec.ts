497b88117684941f17ddb36571488369708ae3eb
Beatrice N <beatrice.nita98@yahoo.com>
2022-08-05 16:27:06 +0300

    Added Booking Service Unit Test

--- src/app/main/services/booking.service.spec.ts
+++ src/app/main/services/booking.service.spec.ts
@@ -1,21 +1,38 @@
-import { TestBed } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { of } from 'rxjs';
+import { fakeAsync, TestBed } from '@angular/core/testing';
 import { BookingService } from './booking.service';
+import { Booking } from '../entities/booking';
 
 describe('BookingService', () => {
   let service: BookingService;
+  let bookingServiceSpy: jasmine.SpyObj<BookingService>;
+
+  let expectedBookings: Booking[] = [
+    { id: 1, rental_id: 1, email: "b98@yahoo.com", nrOfPeople: 5 }
+  ]
 
   beforeEach(() => {
+    bookingServiceSpy = jasmine.createSpyObj('BookingService', ['getBookings']);
+    bookingServiceSpy.getBookings.and.returnValue(of(expectedBookings));
+
     TestBed.configureTestingModule({
-      providers: [ BookingService ],
-      imports: [
-        HttpClientModule
-      ]
+      providers: [{ provide:BookingService, useValue: bookingServiceSpy }]
     });
     service = TestBed.inject(BookingService);
+    bookingServiceSpy = TestBed.inject(BookingService) as jasmine.SpyObj<BookingService>;
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get expected bookings', () => {
+    bookingServiceSpy.getBookings().subscribe({
+      next: bookings => {
+        expect(bookings)
+          .withContext('expected bookings')
+          .toEqual(expectedBookings);
+      }
+    });
   });
 });
